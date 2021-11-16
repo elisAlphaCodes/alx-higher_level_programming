217097940f45fbb9c5a5cb5571401e6a513addff
elisAlphaCodes <70447623+elisAlphaCodes@users.noreply.github.com>
2021-11-16 11:19:09 -0500

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 10):
-    for num == range(number + 1, 10):
-        if number == 8 and num == 9:
-            print("{}{}".format(number, num))
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{}{}".format(number, num), end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
