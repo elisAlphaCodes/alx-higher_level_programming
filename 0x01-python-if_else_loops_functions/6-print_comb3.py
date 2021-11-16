b4335e99dc1e7a847244ddfba71ea7d3fd6bef11
elisALphaCodes <eli.joshua55@gamil.com>
2021-11-16 08:10:01 -0500

    Inventing is a combination of brains and materials

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 10):
+    for num == range(number + 1, 10):
+        if number == 8 and num == 9:
+            print("{}{}".format(number, num))
+        else:
+            print("{}{}".format(number, num), end=", ")
