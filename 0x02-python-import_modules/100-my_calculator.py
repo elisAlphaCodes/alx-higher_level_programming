ed4ad9916f804614bc8005d5e5c3625e234fe856
elisAlphaCodes <70447623+elisAlphaCodes@users.noreply.github.com>
2021-11-18 12:31:26 -0500

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+ 
     from calculator_1 import add, sub, mul, div
     import sys
 
@@ -13,6 +14,6 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
